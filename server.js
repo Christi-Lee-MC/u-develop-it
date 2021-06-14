88adb8980905e33411248383bc836a60d0710e6f
Christi-Lee <christileemc@gmail.com>
2021-06-14 12:23:51 -0700

    created candidate queries

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
 const express = require('express');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+const mysql = require('mysql2');
+
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // Your MySQL username,
+    user: 'root',
+    // Your MySQL password
+    password: 'Sql745417!',
+    database: 'election'
+  },
+  console.log('Connected to the election database.')
+);
 app.use(express.json());
 
 // this is test route to be sure connected.
@@ -13,7 +29,9 @@ app.use(express.json());
 //       message: 'Hello World'
 //     });
 //   });
-
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+  console.log(rows);
+});
   // Default response for any other request (Not Found)
 app.use((req, res) => {
     res.status(404).end();
