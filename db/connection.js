0b59c44f314f5a95b21727b000fa69215b62f032
Christi-Lee <christileemc@gmail.com>
2021-06-16 17:34:29 -0700

    set up routes for candidate and party

---
+++ db/connection.js
@@ -0,0 +1,17 @@
+const mysql = require('mysql2');
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: '',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+  );
+
+//   don't forget to export and then require in server.js
+module.exports = db;
